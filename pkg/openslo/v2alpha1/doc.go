3382bafc8ad95c60b295f4ce93b907a7f9708ee7
Robin Opletal <me@robinopletal.com>
2024-09-30 15:34:40 +0200

    first
    Signed-off-by: Robin Opletal <me@robinopletal.com>

--- pkg/openslo/v2alpha1/doc.go
+++ pkg/openslo/v2alpha1/doc.go
@@ -1,3 +1,4 @@
+// +k8s:deepcopy-gen=package,register
 // Package v2alpha1 contains the OpenSLO specification version v2alpha1 definitions.
 // It is a prototype of the next version of the OpenSLO specification.
 // It is not stable and is subject to breaking changes.
