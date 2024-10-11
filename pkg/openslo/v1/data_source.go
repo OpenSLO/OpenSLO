730dc23cbcfa1971bd781aac6fcc04497071e6c4
Mateusz Hawrus <48822818+nieomylnieja@users.noreply.github.com>
2024-10-11 15:22:42 +0200

    feat: Add v1 DataSource validation (#295)

--- pkg/openslo/v1/data_source.go
+++ pkg/openslo/v1/data_source.go
@@ -3,10 +3,22 @@ package v1
 import (
 	"encoding/json"
 
+	"github.com/nobl9/govy/pkg/govy"
+
+	"github.com/OpenSLO/OpenSLO/internal"
 	"github.com/OpenSLO/OpenSLO/pkg/openslo"
 )
 
 var _ = openslo.Object(DataSource{})
+
+func NewDataSource(metadata Metadata, spec DataSourceSpec) DataSource {
+	return DataSource{
+		APIVersion: APIVersion,
+		Kind:       openslo.KindDataSource,
+		Metadata:   metadata,
+		Spec:       spec,
+	}
+}
 
 type DataSource struct {
 	APIVersion openslo.Version `json:"apiVersion"`
@@ -28,10 +40,26 @@ func (d DataSource) GetName() string {
 }
 
 func (d DataSource) Validate() error {
-	return nil
+	return dataSourceValidation.Validate(d)
 }
 
 type DataSourceSpec struct {
 	Type              string          `json:"type"`
 	ConnectionDetails json.RawMessage `json:"connectionDetails"`
 }
+
+var dataSourceValidation = govy.New(
+	validationRulesAPIVersion(func(d DataSource) openslo.Version { return d.APIVersion }),
+	validationRulesKind(func(d DataSource) openslo.Kind { return d.Kind }, openslo.KindDataSource),
+	validationRulesMetadata(func(d DataSource) Metadata { return d.Metadata }),
+	govy.For(func(d DataSource) DataSourceSpec { return d.Spec }).
+		WithName("spec").
+		Include(govy.New(
+			govy.For(func(spec DataSourceSpec) string { return spec.Type }).
+				WithName("type").
+				Required(),
+			govy.For(func(spec DataSourceSpec) json.RawMessage { return spec.ConnectionDetails }).
+				WithName("connectionDetails").
+				Required(),
+		)),
+).WithNameFunc(internal.ObjectNameFunc[DataSource])
