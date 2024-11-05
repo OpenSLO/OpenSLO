4fd128d70d8228fb29dad213c7f286ed2464bc21
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2024-11-06 00:07:33 +0100

    finish writing down rules

--- pkg/openslo/v1/sli.go
+++ pkg/openslo/v1/sli.go
@@ -1,10 +1,22 @@
 package v1
 
 import (
+	"github.com/OpenSLO/OpenSLO/internal"
 	"github.com/OpenSLO/OpenSLO/pkg/openslo"
+	"github.com/nobl9/govy/pkg/govy"
+	"github.com/nobl9/govy/pkg/rules"
 )
 
 var _ = openslo.Object(SLI{})
+
+func NewSLI(metadata Metadata, spec SLISpec) SLI {
+	return SLI{
+		APIVersion: APIVersion,
+		Kind:       openslo.KindSLI,
+		Metadata:   metadata,
+		Spec:       spec,
+	}
+}
 
 type SLI struct {
 	APIVersion openslo.Version `json:"apiVersion"`
@@ -26,7 +38,7 @@ func (s SLI) GetName() string {
 }
 
 func (s SLI) Validate() error {
-	return nil
+	return sliValidation.Validate(s)
 }
 
 type SLISpec struct {
@@ -35,12 +47,19 @@ type SLISpec struct {
 	RatioMetric     *RatioMetric   `json:"ratioMetric,omitempty"`
 }
 
+type RawMetricType string
+
+const (
+	RawMetricTypeSuccess RawMetricType = "success"
+	RawMetricTypeFailure RawMetricType = "failure"
+)
+
 type RatioMetric struct {
 	Counter bool           `json:"counter"`
 	Good    *SLIMetricSpec `json:"good,omitempty"`
 	Bad     *SLIMetricSpec `json:"bad,omitempty"`
 	Total   *SLIMetricSpec `json:"total,omitempty"`
-	RawType *string        `json:"rawType,omitempty"`
+	RawType RawMetricType  `json:"rawType,omitempty"`
 	Raw     *SLIMetricSpec `json:"raw,omitempty"`
 }
 
@@ -53,3 +72,91 @@ type SLIMetricSource struct {
 	Type             string         `json:"type,omitempty"`
 	MetricSourceSpec map[string]any `json:"spec"`
 }
+
+var sliValidation = govy.New(
+	validationRulesAPIVersion(func(s SLI) openslo.Version { return s.APIVersion }),
+	validationRulesKind(func(s SLI) openslo.Kind { return s.Kind }, openslo.KindSLI),
+	validationRulesMetadata(func(s SLI) Metadata { return s.Metadata }),
+	govy.For(func(s SLI) SLISpec { return s.Spec }).
+		WithName("spec").
+		Include(sliSpecValidation),
+).WithNameFunc(internal.ObjectNameFunc[SLI])
+
+var sliSpecValidation = govy.New(
+	govy.For(func(spec SLISpec) string { return spec.Description }).
+		WithName("description").
+		Rules(rules.StringMaxLength(1050)),
+	govy.For(govy.GetSelf[SLISpec]()).
+		Rules(rules.MutuallyExclusive(true, map[string]func(s SLISpec) any{
+			"thresholdMetric": func(s SLISpec) any { return s.ThresholdMetric },
+			"ratioMetric":     func(s SLISpec) any { return s.RatioMetric },
+		})),
+	govy.ForPointer(func(spec SLISpec) *SLIMetricSpec { return spec.ThresholdMetric }).
+		WithName("thresholdMetric").
+		Include(sliMetricSpecValidation),
+	govy.ForPointer(func(spec SLISpec) *RatioMetric { return spec.RatioMetric }).
+		WithName("ratioMetric").
+		Include(sliRatioMetricValidation),
+)
+
+var sliRatioMetricValidation = govy.New(
+	govy.For(func(m RatioMetric) bool { return m.Counter }).
+		WithName("counter"),
+	govy.For(govy.GetSelf[RatioMetric]()).
+		Cascade(govy.CascadeModeStop).
+		Rules(rules.MutuallyExclusive(true, map[string]func(m RatioMetric) any{
+			"total": func(m RatioMetric) any { return m.Total },
+			"raw":   func(m RatioMetric) any { return m.Raw },
+		})).
+		Include(sliFractionMetricValidation).
+		Include(sliRawMetricSpecValidation),
+)
+
+var sliFractionMetricValidation = govy.New(
+	govy.For(govy.GetSelf[RatioMetric]()).
+		Rules(rules.MutuallyExclusive(true, map[string]func(m RatioMetric) any{
+			"good": func(m RatioMetric) any { return m.Good },
+			"bad":  func(m RatioMetric) any { return m.Bad },
+		})),
+	govy.ForPointer(func(m RatioMetric) *SLIMetricSpec { return m.Total }).
+		WithName("total").
+		Cascade(govy.CascadeModeContinue).
+		Include(sliMetricSpecValidation),
+	govy.ForPointer(func(m RatioMetric) *SLIMetricSpec { return m.Good }).
+		WithName("good").
+		Cascade(govy.CascadeModeContinue).
+		When(func(m RatioMetric) bool { return m.Good != nil }).
+		Include(sliMetricSpecValidation),
+	govy.ForPointer(func(m RatioMetric) *SLIMetricSpec { return m.Bad }).
+		WithName("bad").
+		Cascade(govy.CascadeModeContinue).
+		When(func(m RatioMetric) bool { return m.Bad != nil }).
+		Include(sliMetricSpecValidation),
+).
+	Cascade(govy.CascadeModeStop).
+	When(func(m RatioMetric) bool { return m.Total != nil })
+
+var sliRawMetricSpecValidation = govy.New(
+	govy.ForPointer(func(m RatioMetric) *SLIMetricSpec { return m.Raw }).
+		WithName("raw").
+		Rules(sliMetricSpecValidation),
+	govy.For(func(m RatioMetric) RawMetricType { return m.RawType }).
+		WithName("rawType").
+		Required().
+		Rules(rules.OneOf(RawMetricTypeSuccess, RawMetricTypeFailure)),
+).
+	When(func(m RatioMetric) bool { return m.Raw != nil })
+
+var sliMetricSpecValidation = govy.New(
+	govy.For(func(spec SLIMetricSpec) SLIMetricSource { return spec.MetricSource }).
+		WithName("metricSource").
+		Include(govy.New(
+			govy.For(func(source SLIMetricSource) string { return source.MetricSourceRef }).
+				WithName("metricSourceRef").
+				OmitEmpty().
+				Rules(rules.StringDNSLabel()),
+			govy.For(func(source SLIMetricSource) map[string]any { return source.MetricSourceSpec }).
+				WithName("spec").
+				Required(),
+		)),
+)
---
+++ pkg/openslo/v1/sli_test.go
@@ -0,0 +1,81 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/OpenSLO/OpenSLO/internal/assert"
+	"github.com/OpenSLO/OpenSLO/pkg/openslo"
+	"github.com/nobl9/govy/pkg/govytest"
+	"github.com/nobl9/govy/pkg/rules"
+)
+
+var sliValidationMessageRegexp = getValidationMessageRegexp(openslo.KindSLI)
+
+func TestSLI_Validate_Ok(t *testing.T) {
+	err := validSLI().Validate()
+	govytest.AssertNoError(t, err)
+}
+
+func TestSLI_Validate_VersionAndKind(t *testing.T) {
+	sli := validSLI()
+	sli.APIVersion = "v0.1"
+	sli.Kind = openslo.KindSLO
+	err := sli.Validate()
+	assert.Require(t, assert.Error(t, err))
+	assert.True(t, sliValidationMessageRegexp.MatchString(err.Error()))
+	govytest.AssertError(t, err,
+		govytest.ExpectedRuleError{
+			PropertyName: "apiVersion",
+			Code:         rules.ErrorCodeEqualTo,
+		},
+		govytest.ExpectedRuleError{
+			PropertyName: "kind",
+			Code:         rules.ErrorCodeEqualTo,
+		},
+	)
+}
+
+func TestSLI_Validate_Metadata(t *testing.T) {
+	runMetadataTests(t, "metadata", func(m Metadata) SLI {
+		condition := validSLI()
+		condition.Metadata = m
+		return condition
+	})
+}
+
+func validSLI() SLI {
+	return NewSLI(
+		Metadata{
+			Name:        "search-availability",
+			DisplayName: "Searching availability",
+			Labels: map[string]Label{
+				"team": {"team-a", "team-b"},
+				"env":  {"prod"},
+			},
+		},
+		SLISpec{
+			Description: "X% of search requests are successful",
+			RatioMetric: &RatioMetric{
+				Counter: true,
+				Good: &SLIMetricSpec{
+					MetricSource: SLIMetricSource{
+						MetricSourceRef: "my-datadog",
+						Type:            "Datadog",
+						MetricSourceSpec: map[string]interface{}{
+							"query": "sum:trace.http.request.hits.by_http_status{http.status_code:200}.as_count()",
+						},
+					},
+				},
+				Total: &SLIMetricSpec{
+					MetricSource: SLIMetricSource{
+						MetricSourceRef: "my-datadog",
+						Type:            "Datadog",
+						MetricSourceSpec: map[string]interface{}{
+							"query": "sum:trace.http.request.hits.by_http_status{*}.as_count()",
+						},
+					},
+				},
+			},
+		},
+	)
+}
